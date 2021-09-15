4d3830d53a5094abec9c37d673436bc34a2ae88c
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-09-15 18:39:55 +0600

    Setup All Command

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import io from 'socket.io-client';
+
+class App extends Component {
+
+    componentDidMount() {
+        const socket = io.connect('/');
+    }
+
+    render() {
+        return (
+            <div>
+              <h2>Hello</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
